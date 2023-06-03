8485f75b8b46253a9f2286569b06ad47e582c22e
luka <lukabarbakadze18@gmail.com>
2023-06-03 12:52:38 +0400

    pushing to new repo

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card border border-blue-300 relative">
+    <figure class="px-8 pt-10">
+      <router-link :to="`/product/${product.id}`">
+        <img
+          :src="product.image"
+          alt="Card Image"
+          class="object-contain w-full h-64"
+        />
+      </router-link>
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">
+        <router-link class="link link-hover" :to="`/product/${product.id}`">{{
+          product.title
+        }}</router-link>
+      </h2>
+      <p>{{ toCurrency(product.price) }}</p>
+      <div class="justify-end card-actions">
+        <!-- <button
+      v-if="computed(() => cartStore.contents[product.id]?.quantity === 0)"
+      class="btn btn-primary absolute bottom-3 right-3"
+      @click="cartStore.add(product.id)"
+    >
+      Add to Cart
+    </button> -->
+
+        <div class="flex absolute bottom-3 right-3 gap-x-1">
+          <button
+            @click="cartStore.remove(product.id)"
+            class="scale-125 px-3 py-1 text-sm font-medium text-white bg-red-600 rounded-md"
+          >
+            -
+          </button>
+          <span class="scale-125 px-3 py-1 text-sm font-medium text-black rounded-md">
+            {{ computed(() => cartStore.contents[product.id]?.quantity || 0) }}
+          </span>
+          <button
+            @click="cartStore.add(product.id)"
+            class="scale-125 px-3 py-1 text-sm font-medium text-white bg-blue-600 rounded-md"
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useCartStore } from "../stores/cart";
+import type { Product } from "../stores/products";
+import { toCurrency } from "../utils/formatCurrency";
+
+const cartStore = useCartStore();
+
+defineProps<{
+  product: Product;
+}>();
+</script>
